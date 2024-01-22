f3a988964e9f0dca3811099aefdd549f5dabb540
abel <abelkinfu678@gmail.com>
2024-01-23 00:21:39 +0300

    some change

--- src/features/pages/Prescription.jsx
+++ src/features/pages/Prescription.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import pres1 from "../../assets/pres1.png";
+import pres2 from "../../assets/pres2.jpeg";
+import pres3 from "../../assets/pres2.png";
 
 const Prescription = () => {
   return (
@@ -35,10 +38,7 @@ const Prescription = () => {
             </div>
             <div className="relative flex w-full max-w-[16rem] max-h-[32rem] flex-col rounded-xl bg-gray-600  bg-clip-border shadow-lg">
               <div className="relative mx-4 mt-4 overflow-hidden bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
-                <img
-                  src="https://www.researchgate.net/publication/345830022/figure/fig1/AS:957640024793088@1605330582266/A-sample-prescription-containing-handwritten-texts-over-the-printed-lines.png"
-                  alt="ui/ux review check"
-                />
+                <img src={pres1} alt="ui/ux review check" />
                 <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60" />
               </div>
               <div className="p-6">
@@ -50,10 +50,7 @@ const Prescription = () => {
             </div>
             <div className="relative flex w-full max-w-[16rem] max-h-[32rem] flex-col rounded-xl bg-gray-600  bg-clip-border shadow-lg">
               <div className="relative mx-4 mt-4 overflow-hidden bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
-                <img
-                  src="https://www.researchgate.net/publication/345830022/figure/fig1/AS:957640024793088@1605330582266/A-sample-prescription-containing-handwritten-texts-over-the-printed-lines.png"
-                  alt="ui/ux review check"
-                />
+                <img src={pres2} alt="ui/ux review check" />
                 <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60" />
               </div>
               <div className="p-6">
@@ -65,10 +62,7 @@ const Prescription = () => {
             </div>
             <div className="relative flex w-full max-w-[16rem] max-h-[32rem] flex-col rounded-xl bg-gray-600  bg-clip-border shadow-lg">
               <div className="relative mx-4 mt-4 overflow-hidden bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40">
-                <img
-                  src="https://www.researchgate.net/publication/345830022/figure/fig1/AS:957640024793088@1605330582266/A-sample-prescription-containing-handwritten-texts-over-the-printed-lines.png"
-                  alt="ui/ux review check"
-                />
+                <img src={pres3} alt="ui/ux review check" />
                 <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-tr from-transparent via-transparent to-black/60" />
               </div>
               <div className="p-6">
