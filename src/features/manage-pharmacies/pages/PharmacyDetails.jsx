7c7f91dd98b04160acd6691728ddac29a5c50a26
abel <abelkinfu678@gmail.com>
2024-02-16 00:25:22 +0300

    pharmacy detail page is added

--- src/features/manage-pharmacies/pages/PharmacyDetails.jsx
+++ src/features/manage-pharmacies/pages/PharmacyDetails.jsx
@@ -1,14 +1,77 @@
 function PharmacyDetails() {
-    return (
-      <div className="min-h-screen overflow-hidden flex flex-col flex-shrink-0 antialiased bg-gray-700 text-white">
-        <div className="mx-auto ml-72 w-full overflow-hidden mt-20">
-          <div className="">
-            <h2 className="text-center font-bold mb-3 w-3/4 text-gray-300">Pharmacy detail</h2>
+  return (
+    <div className="min-h-screen overflow-hidden  bg-gray-700 text-white">
+      <div className=" ml-72 overflow-hidden mt-20">
+        <div className="">
+          <div className="bg-gray-900 mr-8  mt-4">
+            <div className="items-center flex py-2 flex-wrap">
+              <div className="w-full md:w-4/12 px-4">
+                <img
+                  alt="..."
+                  className="w-full rounded h-80 shadow-lg"
+                  src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+                />
+              </div>
+              <div className="w-full md:w-5/12  px-4">
+                <div className="md:pr-12">
+                  <h3 className="text-3xl text-gray-300 font-semibold">Rohobot Pharmacy</h3>
+                  <p className="mt-4 text-lg leading-relaxed text-gray-300">
+                    The extension comes with three pre-built pages to help you
+                    get started faster. You can change the text and images and
+                    you're good to go.
+                  </p>
+                  <ul className="list-none mt-6">
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                            <i className="fas fa-fingerprint" />
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-blueGray-500">
+                            Carefully crafted components
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                            <i className="fab fa-html5" />
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-blueGray-500">
+                            Amazing page examples
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                    <li className="py-2">
+                      <div className="flex items-center">
+                        <div>
+                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                            <i className="far fa-paper-plane" />
+                          </span>
+                        </div>
+                        <div>
+                          <h4 className="text-blueGray-500">
+                            Dynamic components
+                          </h4>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    );
-  }
-  
-  export default PharmacyDetails;
-  
+    </div>
+  );
+}
+
+export default PharmacyDetails;
